d2b6d9fc13cebce9c1d7e8c7c47c83b715a0bddf
Tom Wallroth <tomwallroth@gmail.com>
2024-04-10 23:29:37 +0200

    updated owmweather to fetch data after BLE reconnect

--- apps/owmweather/boot.js
+++ apps/owmweather/boot.js
@@ -1,33 +1,28 @@
 {
-  let waiting = false;
+  let fetching = false;
   let settings = Object.assign(
     require('Storage').readJSON("owmweather.default.json", true) || {},
     require('Storage').readJSON("owmweather.json", true) || {}
   );
   
-  let completion = function(){
-    waiting = false;
+  let onCompletedFetch = function(){
+    fetching = false;
     settings.updated = Date.now();
     require('Storage').writeJSON("owmweather.json", settings);
   }
 
-  if (settings.enabled) {    
-    let weather = require("Storage").readJSON('weather.json') || {};
-    if (weather && weather.weather && weather.weather.time) lastUpdate = weather.weather.time;
-
-    if (!settings.updated || settings.updated + settings.refresh * 1000 * 60 < Date.now()){
-      setTimeout(() => {
-        if (!waiting){
-          waiting = true;
-          require("owmweather").pull(completion);
-        }
-      }, 5000);
-    }
-    setInterval(() => {
-      if (!waiting && NRF.getSecurityStatus().connected){
-        waiting = true;
-        require("owmweather").pull(completion);
+  const update = function(){
+    if (!fetching && NRF.getSecurityStatus().connected) {
+      if (!settings.updated || settings.updated + settings.refresh * 1000 * 60 < Date.now()) {
+          fetching = true;
+          require("owmweather").pull(onCompletedFetch);
       }
-    }, settings.refresh * 1000 * 60);
+    }
+  }
+
+  if (settings.enabled) {
+    setTimeout(update, 5000);
+    setInterval(update, settings.refresh * 1000 * 60);
+    NRF.on('connect', update);
   }
 }
