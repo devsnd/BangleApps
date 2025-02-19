cf1b86717957b8fa1adc69a5d882ab4cfe7aa218
Tom Wallroth <tomwallroth@gmail.com>
2025-02-19 16:52:59 +0100

    wip

--- apps/chargeanim2/app.js
+++ apps/chargeanim2/app.js
@@ -1,7 +1,8 @@
 g.setBgColor(0, 0, 0);
 g.clear().flip();
 // 56x88
-var imgbat = require("heatshrink").decompress(atob("nFYhBC/AH4A/AGUeACA22HEo3/G8YrTAC422HBQ2tHBI3/G/43/G/43/G/43/G/43/G/43/G+fTG+vSN+w326Q31GwI3/G9g2WG742CG/43rGwY3yGwg33RKo3bNzQ3bGwo3/G9A2GG942dG/43QGw43uGxA34IKw3VGyY3iG0I3pb8pBRG+wYPG8wYQG/42uG8oZSG/43bDKY3iDKg3cNzI3iRKo3gGyo3/G7A2WG7g2aG/43WGzA3dGzI3/G6fTGzRvcG/43/G/43/G/43/G/43/G/43/G/437HFw2IHFo2KAH4A/AH4Aa"));
+var imgbat = require("heatshrink").decompress(atob("nFYwhC/AHMHu4AGuAbRDQ4ABDf4VNABaENABqSEDSo4FDf4b/Df4b/Df4bd84bZvwb29wbZvwbcU7PuDbI2BDeoaBDbA2CUxIbqKQQbXGwQb0DQSmKDZg2DDeYaDDaw2EDbamKDZY2EAAobPKQoAEHgwblKSDDMR54bOKRYbUYaQbHcRAbuBhAbNUwY2IDdwNJDaA2JDdRSMDaA2KDdgaKDZ42LDZqmBDbYaLDZw2MDdQaMU5wb/Df4b/Df4b/DcMHDStwDYY4WDQgA/AFo="));
+var imgbat2 = require("heatshrink").decompress(atob("nFYwQ/8gWqAAWABo8KBoegBsAKEAAoQClQNJ1ANBBhIABBv4N/BuHqBrOrBpmvBppFM34NM/4NZ1YNM1/+BrO/Bpn/BrJSB/QNK14Na34NM/4NObwgNFKQINLIgIADBph/DBohSBAARHDBogoEBpBHIBpLtLBpzSEBpCaEBqyaEBox/BYZQNPKQgNIdpYNOdpYNCAogNVKQoNHIgoNG1YNNKQoNHBgoNUAA4N/Bv4NplQMJ1ANBhQNJ0ANBAGwA="));
 var imgbubble = require("heatshrink").decompress(atob("i0UhAebgoAFCaYXNBocjAAIWNCYoVHCw4UFIZwqELJQWFKZQVOChYVzABwVaCx7wKCqIWNCg4WMChIXJCZgAnA=="));
 // make black background transparent
 imgbat[2] |= 128;
@@ -11,17 +12,6 @@ imgbubble[3] = 0;
 
 var imgh = 88;
 var imgw = 56;
-// create empty battery image
-var imgbat2 = new ArrayBuffer(imgbat.length);
-for (var i=0; i<imgbat2.length; i++) {
-  if (i > 2) {
-    // overwrite palette, but leave black untouched
-    imgbat2[i] = imgbat[i] ? (imgbat[i] + 13)|0 : imgbat[i];
-  } else {
-    imgbat2[i] = imgbat[i];
-  }
-
-}
 
 var firstBat = E.getBattery();
 var firstTime = getTime();
@@ -34,7 +24,7 @@ var lastFrame = +new Date();
 var frameTime = (1000/25)|0;
 
 var bubbles = [];
-for (var b=0; b<10; b++) {
+for (var b=0; b<4; b++) {
   bubbles.push({y:Math.random()*g.getHeight(),ly:0,x:(0.5+(b<5?b:b+8))*g.getWidth()/18,v:0.6+Math.random(),s:0.5+Math.random()});
 }
 
@@ -71,9 +61,11 @@ function anim() {
     }
     g.drawImage(imgbat, xpos,ypos);
     g.drawString("FULLY\nCHARGED!", cx, cy + 60, true);
+    g.setColor('#fff').setFont('Vector:16').setFontAlign(0,0);
+    g.drawString(require("locale").time(new Date(), 1), cx, 15);
     g.flip();
   } else {
-    for (b=0; b<10; b++) {
+    for (b=0; b<4; b++) {
       g.drawImage(imgbubble, bubbles[b].y, bubbles[b].x, {scale:bubbles[b].s, rotate:Math.PI});
       bubbles[b].y += (bubbles[b].y - (g.getHeight() / 2)) / 100;
       if (bubbles[b].y < -24 || bubbles[b].y > (g.getHeight() + 8)) {
@@ -85,17 +77,17 @@ function anim() {
       }
     }
     g.drawImage(imgbat, xpos, ypos);
-    for (var i=0; i<8; i++) {
-      g.setClipRect(xpos + imgw/8*i, ypos, xpos + imgw/8*(i+1), ypos + imgh - imgh * bat / 100 + ((sintable[((getTime()*8|0)+i)%16]+1)/2) * 10);
+    for (var i=0; i<4; i++) {
+      g.setClipRect(xpos + imgw/4*i, ypos, xpos + imgw/4*(i+1), ypos + imgh - imgh * bat / 100 + ((sintable[((getTime()*8|0)+i)%16]+1)/2) * 10);
       g.drawImage(imgbat2, xpos, ypos);
     }
     g.setClipRect(0, 0, g.getWidth(), g.getHeight());
     g.drawString("CHARGING\n" + bat + '%\n' + eta, cx, cy + 60, true);
+    g.setColor('#fff').setFont('Vector:16').setFontAlign(0,0);
+    g.drawString(require("locale").time(new Date(), 1), cx, 15);
     g.flip();
     setTimeout(anim, Math.max(1, lastFrame + frameTime - new Date()));
   }
-  g.setColor('#fff').setFont('Vector:16').setFontAlign(0,0);
-  g.drawString(require("locale").time(new Date(), 1), cx, 15);
 }
 anim();
 
